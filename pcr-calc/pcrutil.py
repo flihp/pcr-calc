6f04da0fb0524d67455a01be946533f8e09bd2c4
Philip Tricca <flihp@twobit.us>
2013-03-17 20:50:24 -0400

    Remove pcrutil file.

--- pcr-calc/pcrutil.py
+++
@@ -1,7 +0,0 @@
-#
-# Copyright 2013 Philip Tricca <flihp@twobit.us>
-#
-# miscellaneous utility functions used in or related to pcr-calc
-
-# takes a bytearray parameter
-# returns a list of strings as you would see in a hex editor
